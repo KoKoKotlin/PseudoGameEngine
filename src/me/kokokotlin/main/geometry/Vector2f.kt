68a0496a6120edb41d51ce95fd75d45f4c304afe
KoKoKotlin <labruzzler@gmail.com>
2020-02-17 23:37:17 +0100

    Added Vector2f

---
+++ src/me/kokokotlin/main/geometry/Vector2f.kt
@@ -0,0 +1,32 @@
+package me.kokokotlin.main.geometry
+
+import kotlin.math.hypot
+import kotlin.math.roundToInt
+
+class Vector2f(val x: Double, val y: Double) {
+
+    val xI: Int
+        get() = x.roundToInt()
+
+    val yI: Int
+        get() = y.roundToInt()
+
+    infix fun dot(other: Vector2f) = x * other.x + y * other.y
+    infix fun dist(other: Vector2f) = hypot(x - other.x, y - other.y)
+
+    operator fun plus(other: Vector2f) = Vector2f(x + other.x, y + other.y)
+    operator fun times(scalar: Double) = Vector2f(x * scalar, y * scalar)
+    operator fun times(scalar: Int) = Vector2f(x * scalar, y * scalar)
+    operator fun times(other: Vector2f) = this dot other
+    operator fun minus(other: Vector2f) = this + other * -1
+    operator fun div(scalar: Double) = this * (1.0 / scalar)
+    operator fun div(scalar: Int) = this * (1.0 / scalar)
+    operator fun unaryMinus() = this * -1
+
+    override fun toString() = "Vector2f<x: $x, y: $y>"
+    override fun hashCode() = (y.hashCode() + x.hashCode()).hashCode()
+    override fun equals(other: Any?): Boolean {
+        if(other !is Vector2f) return false
+        return x == other.x && y == other.y
+    }
+}
