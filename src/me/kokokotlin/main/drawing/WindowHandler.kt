c9cfa44879f3d2f47191e7f189e8559cfb5cc8d0
Yannik <labruzzler@gmail.com>
2020-06-05 16:05:14 +0200

    Refactored drawing timing and added customizable fps

--- src/me/kokokotlin/main/drawing/WindowHandler.kt
+++ src/me/kokokotlin/main/drawing/WindowHandler.kt
@@ -3,11 +3,10 @@ package me.kokokotlin.main.drawing
 import java.awt.Canvas
 import java.awt.Color
 import java.awt.Dimension
-import java.awt.image.BufferStrategy
 import java.util.concurrent.CopyOnWriteArrayList
 import javax.swing.JFrame
 import javax.swing.JPanel
-import javax.swing.WindowConstants
+import kotlin.math.roundToLong
 
 /*
 * Class that handles drawing in the game and the window
@@ -18,8 +17,23 @@ class WindowHandler(val w0: Int, val h0: Int, title: String) : Canvas(), Runnabl
     private val contentPane = JPanel()      // panel in which the canvas in placed
     private val drawThread = Thread(this)   // thread in which all the drawing happens
 
+    // fps of the game (frames per second that are drawn to the screen)
+    private var currentFps = 60
+    private var optimalFrameTime = 1000000000.0 / fps
+    public var fps: Int
+        get() = currentFps
+        set(v) {
+            currentFps = v
+            optimalFrameTime = 1000000000.0 / fps
+        }
+
+    // last measured real fps of the game
+    private var _lastFps = 0
+    public val lastFps: Int
+        get() = _lastFps
+
     // list of all drawable things in the game
-    val entities = CopyOnWriteArrayList<Drawable>()
+    val drawables = CopyOnWriteArrayList<Drawable>()
 
     init {
         // setup the main window
@@ -49,14 +63,13 @@ class WindowHandler(val w0: Int, val h0: Int, title: String) : Canvas(), Runnabl
     * Redraws the canvas to the buffer
     */
     private fun redraw() {
-
         val g = bufferStrategy.drawGraphics    // graphics context
 
         // clear the whole screen
         g.clearRect(0, 0, w0, h0)
 
         // draw all drawables
-        for (e in entities) {
+        for (e in drawables) {
             e.draw(g)
         }
 
@@ -65,11 +78,32 @@ class WindowHandler(val w0: Int, val h0: Int, title: String) : Canvas(), Runnabl
     }
 
     override fun run() {
-        while (drawThread.isAlive) {
+        var lastLoopTime = System.nanoTime()
+        var lastFpsTime = 0L
+
+        var fps = 0             // real fps counter
+
+        // drawing loop that caps the fps to the desired current_fps value
+        while(drawThread.isAlive) {
+            val now = System.nanoTime()
+            val updateLength = now -lastLoopTime
+            lastLoopTime = now;
+
+            fps++
+
+            lastFpsTime += updateLength
+            if (lastFpsTime >= 1000000000) {
+                lastFpsTime = 0;
+                fps = 0
+            }
+
             redraw()
 
-            // slow down drawing to gain better performance
-            Thread.sleep(10)
+            val sleepTime = ((lastLoopTime - System.nanoTime() + optimalFrameTime) / 1000000).roundToLong()
+            Thread.sleep(when(sleepTime) {
+                0L -> 1L
+                else -> sleepTime
+            })
         }
     }
 
