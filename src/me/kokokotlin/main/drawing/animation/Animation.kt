9586a13812381cc39e3ef71114b080364179221f
Yannik <labruzzler@gmail.com>
2020-06-05 16:28:26 +0200

    Added animation class

---
+++ src/me/kokokotlin/main/drawing/animation/Animation.kt
@@ -0,0 +1,51 @@
+package me.kokokotlin.main.drawing.animation
+
+import java.lang.IllegalArgumentException
+
+/*
+* Class that represents an animation of a drawable object
+*/
+class Animation {
+
+    // keyframes that are shown consecutively on screen during the animation
+    val keyFrames: List<KeyFrame>
+
+    constructor(keyFrames: List<KeyFrame>) {
+        this.keyFrames = keyFrames
+
+        transformToTimeInterval()
+    }
+
+    constructor(sprites: List<Sprite>, durations: List<Double>) {
+        if(sprites.size != durations.size) {
+            throw IllegalArgumentException("Sprites list and duration list must have same lenght")
+        }
+
+        keyFrames = List (sprites.size) {
+            KeyFrame(sprites[it], durations[it])
+        }
+
+        transformToTimeInterval()
+    }
+
+    /*
+    * transforms the duration values of the keyframes into time points
+    * by accumulating them
+    */
+    private fun transformToTimeInterval() {
+        var totalTime = 0.0
+
+        keyFrames.forEach {
+            it.timeInterval = totalTime..(totalTime + it.duration)
+            totalTime += it.duration
+        }
+    }
+
+    /*
+    * returns the sprite that should be drawn in the current time interval
+    */
+    fun getSprite(currentTime: Double) = keyFrames.first { currentTime in it.timeInterval }
+}
+
+data class KeyFrame(val sprite: Sprite, val duration: Double,
+                            var timeInterval: ClosedFloatingPointRange<Double> = 0.0..0.0)
---
+++ src/me/kokokotlin/main/drawing/animation/Sprite.kt
@@ -0,0 +1,8 @@
+package me.kokokotlin.main.drawing.animation
+
+import java.awt.image.BufferedImage
+
+/*
+* Class that represents a drawable image in the game
+*/
+data class Sprite(val image: BufferedImage)
